466a199fb3a135606161596ebdfcdfb6cbf6ecd3
kasaha-la <4k.kasahara@gmail.com>
2020-03-07 20:24:06 +0900

    名前などを修正
    スクリプト、アセットなどの名前に一貫性がなかったので修正

---
+++ Assets/Scripts/Controller/KeyTextController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// 生成された文字自体を処理するクラスです。
+public class KeyTextController : MonoBehaviour
+{
+    // 変数
+    //  コンポーネント変数
+    //  コンポーネント
+    Rigidbody rigidbody;
+    //  内部
+        public bool isEnter = false;
+    private TextMeshPro textMeshPro;
+    float timeToLive = 2.0f;
+    private float currentTime;
+    private Vector3 size;
+    private Color color;
+    private float speed = 0.16f;
+    private float convertMilitoUnitWeight = 1f;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        textMeshPro = this.GetComponent<TextMeshPro>();
+        size = gameObject.transform.localScale;
+        color = textMeshPro.color;
+        currentTime = 0;
+
+        speed += Random.value * 0.05f;
+    }
+
+    void Update()
+    {
+        if (currentTime < timeToLive)
+        {
+            Vector3 position = rigidbody.position;
+            position.y += speed * convertMilitoUnitWeight * Time.deltaTime;
+            rigidbody.MovePosition(position);
+            currentTime += Time.deltaTime;
+            if (!isEnter)
+                gameObject.transform.localScale = size * (timeToLive - currentTime) / timeToLive;
+            else
+            {
+                textMeshPro.color = new Color(color.r, color.g, color.b, 1 * (timeToLive - currentTime) / timeToLive);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/SpaceEffectController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceEffectController : MonoBehaviour
+{
+    // 変数
+    //  コンポーネント変数
+    //  コンポーネント
+    private MeshRenderer mesh;
+    //  内部
+    private float timeToLive;
+    private float currentTime;
+    private Color color;
+    private Vector3 size;
+    private float speed = 0.26f;
+    private float convertMilitoUnitWeight = 1f;
+
+    void Start()
+    {
+        size = gameObject.transform.localScale;
+        mesh = GetComponent<MeshRenderer>();
+        color = mesh.material.color;
+        currentTime = 0;
+        timeToLive = 0.7f;
+
+        speed += Random.value * 0.05f;
+    }
+
+    void Update()
+    {
+        if (currentTime < timeToLive)
+        {
+            Vector3 position = gameObject.transform.position;
+            position.y += speed * convertMilitoUnitWeight * Time.deltaTime;
+            gameObject.transform.position = position;
+            currentTime += Time.deltaTime;
+            Vector3 nowSize = new Vector3(size.x, size.y * (timeToLive - currentTime) / timeToLive, size.z);
+            gameObject.transform.localScale = nowSize;
+
+            mesh.material.color = new Color(color.r, color.g, color.b, 1 * (timeToLive - currentTime) / timeToLive);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
